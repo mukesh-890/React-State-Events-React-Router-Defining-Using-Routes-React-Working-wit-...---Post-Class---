ad143434effbe5e52d0595958d65f0ca633c36e5
Mukesh Rajbanshi <59588637+mukesh-890@users.noreply.github.com>
2022-06-10 00:36:08 +0530

    initial commit

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import {Home} from "./Pages/Home.js"
+import {Index} from "./Pages/Index.js"
+import {NotFound} from "./Pages/NotFound.js"
+import {Routes,Route} from 'react-router-dom';
 export const AppRoutes = () => {
     return (
-
+        <>
+        <Routes>
+        <Route path='/' element={<Index/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='*' element={<NotFound/>}/>
+        </Routes>
+        </>
     )
 }
